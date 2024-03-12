397bcceabe723f475af2e0b99878f09940d641c0
Sierra!~ <48463140+9u3@users.noreply.github.com>
2024-03-12 14:44:37 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'Rolimons API',
+  packages = ['rAPI'],
+  version = '1.06',
+  license='MIT',
+  description = 'API to interact with Rolimon's',
+  author = 'Nox Protogen',
+  author_email = 'cfym.gamer@gmail.com',
+  url = 'https://github.com/9u3/rAPI/',
+  download_url = 'https://github.com/9u3/rAPI/tree/master/rAPI',
+  keywords = ['wrapper', 'api', 'rolimons', 'roblox'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
